2c1a62cd0337433e8a7014084f709786d126c115
grotixxx <egorpro01052007@gmail.com>
2022-09-02 21:26:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from module import x_y
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import show
+import numpy as np
+
+x = np.linspace(0, 10, 100)  # Sample data.
+plt.plot(x_y)
+plt.show()
---
+++ module.py
@@ -0,0 +1,6 @@
+import random
+def add(x: int, y: int):
+    return x, y
+x = random.randint(0, 100)
+y = random.randint(0, 100)
+x_y = add(x, y)
